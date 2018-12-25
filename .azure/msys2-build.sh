4c8caf691bf4be39faf6bec4a3797db33603947f
Nelson Chen <crazysim@gmail.com>
2018-12-24 20:21:42 -0800

    azure-pipelines: Build and save x86 and x64

--- .azure/msys2-build.sh
+++ .azure/msys2-build.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+# Write to an out directory
+mkdir -p out
+
 # Link DLLS for copying
 if [ "$MSYS2_ARCH" = "x86_64" ]; 
 then
@@ -10,6 +13,13 @@ then
   ln -sfn /mingw64/bin/libcairo-2.dll dll/w64/libcairo-2.dll
   ln -sfn /mingw64/bin/libgdk-3-0.dll dll/w64/libgdk-3-0.dll
   ln -sfn /mingw64/bin/libgdk_pixbuf-2.0-0.dll dll/w64/libgdk_pixbuf-2.0-0.dll
+  ln -sfn /mingw64/bin/libglib-2.0-0.dll dll/w64/libglib-2.0-0.dll
+  # Include WHPX
+  WINSDK="/c/Program Files (x86)/Windows Kits/10"
+  WINSDKVER=10.0.17134.0
+  cp "$WINSDK/Include/$WINSDKVER/um/"WinHv* /mingw64/include
+  cp "$WINSDK/Lib/$WINSDKVER/um/x64/"WinHv* /mingw64/lib
+  cp dll/w64/*.dll out/
 elif [ "$MSYS2_ARCH" = "i686" ];
  then
   mkdir -p dll/w32
@@ -17,6 +27,8 @@ elif [ "$MSYS2_ARCH" = "i686" ];
   ln -sfn /mingw32/bin/libcairo-2.dll dll/w32/libcairo-2.dll
   ln -sfn /mingw32/bin/libgdk-3-0.dll dll/w32/libgdk-3-0.dll
   ln -sfn /mingw32/bin/libgdk_pixbuf-2.0-0.dll dll/w32/libgdk_pixbuf-2.0-0.dll
+  ln -sfn /mingw32/bin/libglib-2.0-0.dll dll/w32/libglib-2.0-0.dll
+  cp dll/w32/*.dll out/
 fi 
 
 # Stack protector disabled for windows to work around crashing issue
@@ -24,15 +36,21 @@ fi
 ./configure \
   --enable-gtk \
   --enable-sdl \
+  --enable-whpx \
   --target-list=x86_64-softmmu \
   --prefix=out \
   --disable-stack-protector \
   --disable-werror
 
-make -j 2
+MAKE_JOB_COUNT="${NUMBER_OF_PROCESSORS:-2}"
+
+make -j "$MAKE_JOB_COUNT"
 
 file x86_64-softmmu/qemu-system-x86_64.exe
 
 x86_64-softmmu/qemu-system-x86_64.exe --version
+
+cp x86_64-softmmu/qemu-system-x86_64.exe out/
+cp x86_64-softmmu/qemu-system-x86_64w.exe out/
 
 make install
--- .azure/msys2-install.sh
+++ .azure/msys2-install.sh
@@ -9,8 +9,8 @@ pacman --noconfirm -Suy
 pacman --noconfirm -S --needed \
     base-devel \
     git \
-    mingw-w64-$MSYS2_ARCH-glib2 \
-    mingw-w64-$MSYS2_ARCH-toolchain \
-    mingw-w64-$MSYS2_ARCH-gtk3 \
-    mingw-w64-$MSYS2_ARCH-SDL2 \
+    mingw-w64-"$MSYS2_ARCH"-glib2 \
+    mingw-w64-"$MSYS2_ARCH"-toolchain \
+    mingw-w64-"$MSYS2_ARCH"-gtk3 \
+    mingw-w64-"$MSYS2_ARCH"-SDL2 \
     python
